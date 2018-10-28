e761e01e223099cbf91df915c99ed1be5a644eef
Erik Martines Sanches <erik.martinessanches@gmail.com>
2018-10-28 07:18:41 +0100

    Changing the Pie model and updating the database with a new migration.

--- BethysPieShop/Models/Pie.cs
+++ BethysPieShop/Models/Pie.cs
@@ -15,5 +15,6 @@ namespace BethysPieShop.Models
         public string ImageUrl { get; set; }
         public string ImageThumbnailUrl { get; set; }
         public bool IsPieOfTheWeek { get; set; }
+        public bool IsInStock { get; set; }
     }
 }
