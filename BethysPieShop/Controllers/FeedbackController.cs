059ea0fb3964870f4e63a28cfa1f0fdb03998154
Erik Martines Sanches <erik.martinessanches@gmail.com>
2018-10-28 08:57:17 +0100

    Validation the feedback form.

--- BethysPieShop/Controllers/FeedbackController.cs
+++ BethysPieShop/Controllers/FeedbackController.cs
@@ -20,5 +20,22 @@ namespace BethysPieShop.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Index(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+                _feedbackRepository.AddFeedback(feedback);
+                return RedirectToAction("FeedbackComplete");
+            }
+            //Return the user to the form with the same data still in it, if validation fails.
+            return View(feedback);
+        }
+
+        public IActionResult FeedbackComplete()
+        {
+            return View();
+        }
     }
 }
